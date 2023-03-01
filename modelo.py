a4a575db487f81e18e20d7913e55aca27eae0cb0
Bruna Santiago <brunasanthiago@outlook.com>
2023-03-01 14:04:31 -0300

    refatorando e implementando as classes

--- modelo.py
+++ modelo.py
@@ -18,30 +18,35 @@ class Programa:
     @nome.setter
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
+    
+    def imprime(self):
+        print(f'Nome: {self.nome} - Ano: {self.ano} - Likes: {self.likes}')
 
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self._nome = nome.title()
-        self.ano = ano
-        self._likes = 0
+        super().__init__(nome, ano)
         self.duracao = duracao
+
+    def imprime(self):
+        print(f'Nome: {self.nome} - Ano: {self.ano} - Duração: {self.duracao} min- Likes: {self.likes}')
 
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self._nome = nome.title()
-        self.ano = ano
-        self._likes = 0
+        super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def imprime(self):
+        print(f'Nome: {self.nome} - Ano: {self.ano} - Temporadas: {self.temporadas}- Likes: {self.likes}')
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f'{vingadores.nome} - {vingadores.ano} - {vingadores.duracao} min - {vingadores.likes} likes')
-
 
 atlanta = Serie('Atlanta', 2018, 2)
 atlanta.dar_likes()
 
+filme_e_serie = [vingadores, atlanta]
 
-print(f'{atlanta.nome} - {atlanta.temporadas} temporadas - {atlanta.likes} likes')
+for programa in filme_e_serie:
+    detalhes = programa.imprime()
