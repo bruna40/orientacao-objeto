6b1d82cae1af0120d3406481add7281b2941762e
Bruna Santiago <brunasanthiago@outlook.com>
2023-03-01 19:02:51 -0300

    entendendo duck typing

--- modelo.py
+++ modelo.py
@@ -19,8 +19,8 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
     
-    def imprime(self):
-        print(f'Nome: {self.nome} - Ano: {self.ano} - Likes: {self.likes}')
+    def __str__(self):
+        return f'Nome: {self.nome} - Ano: {self.ano} - Likes: {self.likes}'
 
 
 class Filme(Programa):
@@ -28,8 +28,8 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f'Nome: {self.nome} - Ano: {self.ano} - Duração: {self.duracao} min- Likes: {self.likes}')
+    def __str__(self):
+        return f'Nome: {self.nome} - Ano: {self.ano} - Duração: {self.duracao} min- Likes: {self.likes}'
 
 
 class Serie(Programa):
@@ -37,16 +37,48 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f'Nome: {self.nome} - Ano: {self.ano} - Temporadas: {self.temporadas}- Likes: {self.likes}')
+    def __str__(self):
+        return f'Nome: {self.nome} - Ano: {self.ano} - Temporadas: {self.temporadas}- Likes: {self.likes}'
+
+
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __len__(self):
+        return len(self._programas)
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-
 atlanta = Serie('Atlanta', 2018, 2)
+tmep = Filme('Todo mundo em panico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+
+atlanta.dar_likes()
+tmep.dar_likes()
+atlanta.dar_likes()
+vingadores.dar_likes()
+atlanta.dar_likes()
+demolidor.dar_likes()
+atlanta.dar_likes()
+vingadores.dar_likes()
 atlanta.dar_likes()
 
-filme_e_serie = [vingadores, atlanta]
+filme_e_serie = [vingadores, atlanta, demolidor, tmep]
+fim_de_semana = Playlist('fim de semana', filme_e_serie)
 
-for programa in filme_e_serie:
-    detalhes = programa.imprime()
+print(f'Tamanho da playlist: {len(fim_de_semana)}')
+
+for programa in fim_de_semana:
+    print(programa)
+    
+print(f'Tá ou não tá? {demolidor in fim_de_semana}')
